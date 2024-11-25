86d34d40367ce4ee5dae7fc172340620bb8f548f
DeveloperImran1 <abdulquddus01925251187@gmail.com>
2024-11-26 01:15:57 +0600

    live site link added in cors

--- api/app.js
+++ api/app.js
@@ -22,7 +22,7 @@ const orderRoute = require("../src/routes/orders/order.route");
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: ["http://localhost:3000", "http://localhost:3001", "https://book-vila-client.vercel.app"],
     credentials: true,
 }))
 
